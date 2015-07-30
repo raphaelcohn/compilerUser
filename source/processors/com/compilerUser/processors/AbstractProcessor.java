6f5af924be900ab84645a708fddab35e9e81e13a
Raphael Cohn <raphael.cohn@stormmq.com>
2015-07-30 11:47:36 +0100

    Added copyright

--- source/processors/com/compilerUser/processors/AbstractProcessor.java
+++ source/processors/com/compilerUser/processors/AbstractProcessor.java
@@ -1,3 +1,8 @@
+/*
+ * This file is part of compilerUser. It is subject to the licence terms in the COPYRIGHT file found in the top-level directory of this distribution and at https://raw.githubusercontent.com/raphaelcohn/compilerUser/master/COPYRIGHT. No part of compilerUser, including this file, may be copied, modified, propagated, or distributed except according to the terms contained in the COPYRIGHT file.
+ * Copyright © 2014-2015 The developers of compilerUser. See the COPYRIGHT file in the top-level directory of this distribution and at https://raw.githubusercontent.com/raphaelcohn/compilerUser/master/COPYRIGHT.
+ */
+
 package com.compilerUser.processors;
 
 import com.sun.source.util.Trees;
